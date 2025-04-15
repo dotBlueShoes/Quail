f7dd39e8a2f1e51a09f0815c6deed13a700b79fb
Matthew <40423487+dotBlueShoes@users.noreply.github.com>
2025-04-16 00:28:12 +0200

    Create colors.hpp

---
+++ project/quail/inc/colors.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <blue/error.hpp>
+
+//namespace COLORS {
+
+	//  TODO
+	// Theres prob. a different default color within different console settings.
+	//
+	const u8 COLOR_DEFAULT 	=  7;
+	const u8 COLOR_WHITE 	= 15;
+	const u8 COLOR_YELLOW 	= 14;
+	const u8 COLOR_BLUE 	= 11;
+	const u8 COLOR_RED 		= 12;
+
+
+//}
